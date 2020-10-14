87566cf3f1c72c2e5ca5b96ecac2449e32353f10
ramy <ramydoki@gmail.com>
2020-10-14 13:42:14 +0200

    Desk decoration + fan script

---
+++ Assets/paperScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paperScript : MonoBehaviour
+{
+
+        float spinSpeed = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown(){ 
+        transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
