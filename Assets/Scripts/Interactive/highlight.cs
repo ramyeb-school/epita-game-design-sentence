f152d543915001456ec858311abd752cdcd15333
Ewskr0 <ewskr0@gmail.com>
2020-10-14 16:33:50 +0200

    Adds higlight script

---
+++ Assets/Scripts/Interactive/highlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class highlight : MonoBehaviour
+{
+    // Fade the color from the orignial to yellow
+    // back and forth over the defined duration
+
+    Color colorStart;
+    Color colorEnd = Color.yellow;
+    void Awake()
+    {
+        colorStart = GetComponent<Renderer>().material.color;
+    }
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.Lerp(colorStart, colorEnd, 0.5f);
+    }
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = colorStart;
+    }
+
+}
