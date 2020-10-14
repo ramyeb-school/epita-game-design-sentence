40c5b979eeb7af5d219773488810d1ce952fb3ed
Ewskr0 <ewann.villa@gmail.com>
2020-10-14 17:55:04 +0200

    Adds levelManager

---
+++ Assets/Scripts/Interactive/document.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class document : MonoBehaviour
+{
+    // Used for highligthing.
+    [SerializeField] private Color highlightColor = Color.yellow;
+    private Renderer ownRenderer = null;
+    private Color[] originalColors;
+
+    // Used to display document.
+    private GameObject levelManager;
+    private void Start()
+    {
+        if (ownRenderer == null) { ownRenderer = GetComponent<Renderer>(); }
+        StoreOriginalColor();
+        if (levelManager == null) { levelManager = GameObject.FindGameObjectWithTag("levelManager"); }
+    }
+    private void StoreOriginalColor()
+    {
+        if (ownRenderer != null)
+        {
+            Material[] materials = ownRenderer.materials;
+            originalColors = new Color[materials.Length];
+            for (int i = 0; i < materials.Length; ++i) { originalColors[i] = materials[i].color; }
+        }
+    }
+
+
+    private void OnMouseEnter()
+    {
+        if (ownRenderer != null)
+        {
+            Material[] materials = ownRenderer.materials;
+            for (int i = 0; i < materials.Length; ++i) { materials[i].color = Color.Lerp(materials[i].color, highlightColor, 0.5f); }
+            ownRenderer.materials = materials;
+        }
+    }
+    private void OnMouseExit()
+    {
+        if (ownRenderer != null)
+        {
+            Material[] materials = ownRenderer.materials;
+            for (int i = 0; i < materials.Length; ++i) { materials[i].color = originalColors[i]; }
+            ownRenderer.materials = materials;
+        }
+    }
+
+        public void OnMouseDown()
+    {
+        if(levelManager != null) {
+            levelManager.GetComponent<levelManager>().ShowDocument();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Interactive/levelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelManager : MonoBehaviour
+{
+
+    private GameObject panelDocument;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (panelDocument == null) { panelDocument = GameObject.FindGameObjectWithTag("panelDocument"); }
+        HideDocument();
+    }
+
+    public void ShowDocument()
+    {
+        if (panelDocument != null)
+            panelDocument.SetActive(true);
+
+    }
+
+    public void HideDocument()
+    {
+        if (panelDocument != null)
+            panelDocument.SetActive(false);
+
+    }
+}
