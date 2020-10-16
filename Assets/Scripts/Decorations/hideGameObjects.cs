d064db4d11df0b2dba3312be8c4a13d7f32bfde3
Ewskr0 <ewann.villa@gmail.com>
2020-10-16 17:08:22 +0200

    Adds clean scene

---
+++ Assets/Scripts/Decorations/hideGameObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class hideGameObjects : MonoBehaviour
+{
+    public GameObject[] hide;
+
+    void Update()
+    {
+        foreach (GameObject go in hide)
+        {
+
+            if (Application.isPlaying)
+                go.SetActive(true);
+            else
+                go.SetActive(false);
+        }
+    }
+}
